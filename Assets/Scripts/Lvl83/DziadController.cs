29e3217a832158eb500bf91b2944a3d140c91288
S0l4k <lukasz.drobik6@gmail.com>
2025-02-08 20:39:41 +0100

    afafaef

--- Assets/Scripts/Lvl83/DziadController.cs
+++ Assets/Scripts/Lvl83/DziadController.cs
@@ -5,21 +5,43 @@ public class DziadController : MonoBehaviour
     private Rigidbody2D dziadRigidbody;
     private BoxCollider2D dziadCollider;
     private Vector2 movementDirection;
-
     public float initialMovementSpeed = 1f;
     public float minMovementSpeed = 0.3f;
     [SerializeField] private float speedDecayRate = 0.01f;
-    private bool isMoving;
+    private bool isWalking;
+    private Animator animator;
 
     void Start()
     {
+        // Pobierz komponenty
+        animator = GetComponent<Animator>();
         dziadRigidbody = GetComponent<Rigidbody2D>();
         dziadCollider = GetComponent<BoxCollider2D>();
+
+        // Ustaw domyślną wartość isWalking
+        isWalking = false;
+        animator.SetBool("isWalking", isWalking);
     }
 
     void Update()
     {
+        // Aktualizuj wejście gracza
         ProcessInputs();
+
+        // Wykrywanie ruchu gracza
+        if (movementDirection != Vector2.zero)
+        {
+            isWalking = true; // Gracz się rusza
+        }
+        else
+        {
+            isWalking = false; // Gracz stoi nieruchomo
+        }
+
+        // Aktualizacja animacji
+        animator.SetBool("isWalking", isWalking);
+
+        // Spowalnianie prędkości
         if (initialMovementSpeed > minMovementSpeed)
         {
             initialMovementSpeed -= speedDecayRate * Time.deltaTime;
@@ -34,6 +56,7 @@ public class DziadController : MonoBehaviour
 
     public void ProcessInputs()
     {
+        // Pobierz wejście gracza
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         movementDirection = new Vector2(moveX, moveY).normalized;
