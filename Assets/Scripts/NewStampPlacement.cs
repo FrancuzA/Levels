b42a27328d50a9e52f59d808944bacc83bdd2a1c
Cyprian "Francuz" Arquier <149786616+FrancuzA@users.noreply.github.com>
2025-02-08 03:37:12 +0100

    lvl25

---
+++ Assets/Scripts/NewStampPlacement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class NewStampPlacement : MonoBehaviour
+{
+    public float minX = -0.3f; // Left boundary of the paper
+    public float maxX = 0.3f;  // Right boundary of the paper
+    public Transform stampPlace; // Reference to the stamp place object
+    public static NewStampPlacement Instance;
+
+    void Start()
+    {
+        GenerateRandomStampPlace();
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateRandomStampPlace();
+        }
+    }
+    public void GenerateRandomStampPlace()
+    {
+        if (stampPlace != null)
+        {
+            float randomX = Random.Range(minX, maxX);
+            stampPlace.position = new Vector3(randomX, stampPlace.position.y, stampPlace.position.z);
+        }
+    }
+}
