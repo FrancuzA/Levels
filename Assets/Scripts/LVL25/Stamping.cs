bb356473b505d8297625a70fee20fd6752e80512
FrancuzA <149786616+FrancuzA@users.noreply.github.com>
2025-02-08 22:49:11 +0100

    stampi stamp

--- Assets/Scripts/LVL25/Stamping.cs
+++ Assets/Scripts/LVL25/Stamping.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Stamping : MonoBehaviour
 {
     public GameObject StampPrefab;
+    public GameObject Paper;
     public Transform hand; // Reference to the hand object
     public Transform stampPlace; // Reference to the stamp place object
     public TMP_Text scoreText; // Reference to the TextMeshPro UI text
@@ -16,6 +18,7 @@ public class Stamping : MonoBehaviour
     public EventReference StampReferance;
     private EventInstance StampInstance;
     public Animator Anim;
+    private bool stamping = false;
 
     private void Start()
     {
@@ -25,17 +28,21 @@ public class Stamping : MonoBehaviour
     void Update()
     {
         Distance = Mathf.Abs(hand.position.x - stampPlace.position.x);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !stamping)
         {
             Anim.SetTrigger("Stamp");
             HandleStamp();
             StampInstance.start();
         }
-        
+        if (score == 10)
+        {
+            SceneManager.LoadScene(5);
+        }
     }
 
     void HandleStamp()
     {
+        StartCoroutine(StampStamp());
         if (Distance<=tolerance)
         {
             score++;
@@ -56,12 +63,16 @@ public class Stamping : MonoBehaviour
         }
     }
 
-    IEnumerator StmpStamp()
-    {/*
-        GameObject newstamp Instantiate(StampPrefab,);
-        New
-        
-        NewStampPlacement.Instance.GenerateRandomStampPlace();*/
+    IEnumerator StampStamp()
+    {
+        stamping = true;
+        yield return new WaitForSeconds(0.5f);
+        GameObject newstamp = Instantiate(StampPrefab,new Vector3(hand.transform.position.x + 0.25f,stampPlace.transform.position.y,-0.2f),stampPlace.rotation);
+        newstamp.transform.SetParent(Paper.transform);
+        yield return new WaitForSeconds(1.5f);
+        NewStampPlacement.Instance.GenerateRandomStampPlace();
+        Destroy(newstamp);
+        stamping = false;   
         yield return null;
     }
 }
